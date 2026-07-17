7ac96f79402662484fb7e794d7b82af3062ba7f9
Priyanka M S <priyankamanni91@gmail.com>
2026-07-17 15:03:58 +0530

    Initial commit

---
+++ class.py
@@ -0,0 +1,61 @@
+'''for i i range(1,5):
+    total = total * i
+print(total)
+
+
+
+while i < 5:
+    total=total * i
+    i=i+1
+print(total)
+
+
+num=int(input("Enter the Number"))
+count=0
+for digit in num:
+'''
+'''
+count=0
+num=input("Enter the Number")
+for digit in num:
+    count=count+1
+print(coun
+sum=0
+num=input("Enter the Number")
+
+for dig in num:
+    sum=sum+int(dig)
+print(sum)
+
+
+
+import pandas as pd
+df=pd.read_csv("emp.csv")
+type(df)
+print(df)
+while num>0:
+    num=num//10
+    count=count+1
+print(count)
+
+num= 12345
+count=0
+while num>0:
+    num=num//10
+    count=count+1
+print(count)
+'''
+num= 12345
+count=0
+for dig in str(num):
+    count=count+1
+print(count)
+
+
+
+
+
+
+
+
+
